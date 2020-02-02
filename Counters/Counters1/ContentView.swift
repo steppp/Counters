5868d8efd86d2fd4a65994266cb34ef984f2ee35
Stefano Andriolo <stefano.andriolo3@studio.unibo.it>
2020-02-02 10:41:28 +0100

    Create Xcode project

---
+++ Counters/Counters1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Counters
+//
+//  Created by Stefano Andriolo on 02/02/2020.
+//  Copyright © 2020 Stefano Andriolo. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
